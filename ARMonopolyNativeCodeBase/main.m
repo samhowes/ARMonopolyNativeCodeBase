6f1c75adecc85f49219bd6b0059ac51ad55d67a8
Sam Howes <showes06@gmail.com>
2014-03-03 11:44:19 -0500

    Initial Commit

---
+++ ARMonopolyNativeCodeBase/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ARMonopolyNativeCodeBase
+//
+//  Created by Sam Howes on 3/3/14.
+//  Copyright (c) 2014 Sam Howes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ARMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ARMAppDelegate class]));
+    }
+}
