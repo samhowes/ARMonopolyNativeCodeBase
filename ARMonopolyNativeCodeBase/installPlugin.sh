2c2dd811efb3d7c4659204c00808107aae7e7342
Sam Howes <showes06@gmail.com>
2014-03-29 22:00:42 -0400

    Updated Integration Scripts Issue #22 Changes: 	- installPlugin.sh now accepts a command line argument. When that is activated, it will copy the project file from the unity folder to the /integration folder. 	- It also copies this file to the destination xcodeproj package, and appends ‘.replaced’ to the old project file name.

--- ARMonopolyNativeCodeBase/installPlugin.sh
+++ ARMonopolyNativeCodeBase/installPlugin.sh
@@ -6,37 +6,37 @@
 #  Created by Samuel Howes on 2/1/14.
 #  Copyright (c) 2014 Samuel Howes. All rights reserved.
 STORYBOARD="Base.lproj/Main.storyboard"
-SETTINGS_CONTROLLERS="\
-ARMSettingsViewController.h     ARMSettingsViewController.m     \
-ARMEditProfileViewController.h  ARMEditProfileViewController.m  \
-ARMBluetoothViewController.h    ARMBluetoothViewController.m    \
-ARMNetworkViewController.h      ARMNetworkViewController.m      \
-"
-BLUETOOTH="\
-LeDiscovery.h                   LeDiscovery.m                   \
-"
-DATA_MODEL="\
-ARMPlayerInfo.h                 ARMPlayerInfo.m                 \
-"
-DEPLOY_ONLY="\
-ARMAppController.h              ARMAppController.m              \
-"
-#CLASSES="\
-#ARMPlayViewController.h         ARMPlayViewController.m         \
-#$DATA_MODEL $SETTINGS_CONTROLLERS $BLUETOOTH $DEPLOY_ONLY"
 
-CLASSES="*.m *.h"
+CLASSES="ARM*.m ARM*.h"
 
 LOGO="LOGO.png"
 
-
 COPY_FILES="$STORYBOARD $CLASSES $LOGO"
-COMPILE_DIR="../../ARMonopolyProduct/Libraries/"
+COMPILE_DIRNAME="../../ARMonopolyProduct/Libraries"
 
-### Copy all the files over
-for file in $COPY_FILES
-do
-	cp $file $COMPILE_DIR
-done
+PROJECT_FILENAME="project.pbxproj"
+UNITY_PROJECT_FILE="$COMPILE_DIRNAME/../Unity-iPhone.xcodeproj/$PROJECT_FILENAME"
+UNITY_COPY_TO="integration/$PROJECT_FILENAME"
 
+if [ $1 ]; then
+    echo "Copying Unity Project file to integration/ directory..."
+    cp $UNITY_PROJECT_FILE $UNITY_COPY_TO
+
+else
+    echo "Copying classes into Unity Project folder at '$COMPILE_DIRNAME'..."
+    ### Copy all the files over
+    for file in $COPY_FILES
+    do
+        echo "Copying '$file'..."
+        cp $file "$COMPILE_DIRNAME/"
+    done
+
+    echo "Copying project file to '$UNITY_PROJECT_FILE'..."
+
+    mv $UNITY_PROJECT_FILE "$UNITY_PROJECT_FILE.replaced"
+    cp $UNITY_COPY_TO $UNITY_PROJECT_FILE
+
+fi
+
+echo "Done!"
 ### Plugin installed!
